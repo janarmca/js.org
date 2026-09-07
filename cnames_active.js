e17cbffcc5dd88cb5d2151451a330689b0f9d1f8
janarmca <50902409+janarmca@users.noreply.github.com>
2026-09-07 15:31:16 +0530

    Update cnames_active.js

--- cnames_active.js
+++ cnames_active.js
@@ -3869,6 +3869,7 @@ var cnames_active = {
   "zylog": "cname.vercel-dns.com", // noCF
   "zyx": "zyx.alwaysdata.net",
   "zyy": "zyyou.github.io/notes"
+  "pdf-tools": "pdf-tools-backend-lime.vercel.app",
   /*
    * please don't add your subdomain records down here!
    * insert them in alphabetical order to help reduce merge conflicts.
